8262053614c2c6c1a002778a533dfb108b18d574
Oladayo234 <oladayoolubunmi20@gmail.com>
2026-04-22 08:32:16 +0100

    Files and IO

--- test/FileDataWriterTest.java
+++ test/FileDataWriterTest.java
@@ -25,10 +25,15 @@ public class FileDataWriterTest {
 
     @Test
     public void readDataFromFile(){
-        String fileLocation = "/home/semi/Documents/Semicolon/Phase_Two/Intelij Projects/JavaIO/src/output";
-        Path path = Paths.get(fileLocation, "numbers.txt");
-        String readFile = ReadFromFile.readDataFromFile(path);
-        assertEquals("10,100,1000,10000", readFile);
+        try{
+            String fileLocation = "/home/semi/Documents/Semicolon/Phase_Two/Intelij Projects/JavaIO/src/output";
+            Path path = Paths.get(fileLocation, "numbers.txt");
+            Files.writeString(path, "10,100,1000,10000");
+            String readFile = ReadFromFile.readDataFromFile(path);
+            assertEquals("10,100,1000,10000", readFile);
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 
     @Test
@@ -41,19 +46,29 @@ public class FileDataWriterTest {
 
     @Test
     public void deleteDirectory(){
-        String fileLocation = "/home/semi/Documents/Semicolon/Phase_Two/Intelij Projects/JavaIO/src/output/folderOne/folderTwo";
-        Path path = Paths.get(fileLocation);
-        Path path2 = Path.of(fileLocation, "folderOne", "folderTwo");
-        ReadFromFile.deleteFile(path);
-        assertFalse(Files.exists(path));
+        try {
+            String fileLocation = "/home/semi/Documents/Semicolon/Phase_Two/Intelij Projects/JavaIO/src/output/folderOne/folderTwo";
+            Path path = Paths.get(fileLocation);
+            Files.createDirectories(path);
+            assertTrue(Files.exists(path));
+            ReadFromFile.deleteFile(path);
+            assertFalse(Files.exists(path));
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 
     @Test
     public void updateDataToFile(){
-        String fileLocation = "/home/semi/Documents/Semicolon/Phase_Two/Intelij Projects/JavaIO/src/output/numbers.txt";
-        Path path = Paths.get(fileLocation);
-        String readFile = ReadFromFile.updateFile(path, "He is a Boy");
-        assertEquals("10,100,1000,10000 He is a Boy", readFile);
+        try{
+            String fileLocation = "/home/semi/Documents/Semicolon/Phase_Two/Intelij Projects/JavaIO/src/output/numbers.txt";
+            Path path = Paths.get(fileLocation);
+            String contentBefore = Files.readString(path);
+            String updatedContent = ReadFromFile.updateFile(path, "He is a Boy");
+            assertEquals(contentBefore + " He is a Boy", updatedContent);
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 
 }
